5a1e481a0c5386bed2e732c2bae188f700bb7a47
Emanuele Pietropaolo <edoppiap@gmail.com>
2023-12-12 17:36:33 +0100

    Implemented chi squared test (but it returns 1)

--- LabL5/labl5.py
+++ LabL5/labl5.py
@@ -24,28 +24,66 @@ import random
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from scipy.special import factorial
+import scipy.stats as stats
 
 P_THRESHOLD = .01
 
 SIM_TIME = 1_000_000
 
+random.seed(42)
+np.random.seed(42)
+
+def degree_of(node):
+    return len(node['neighbors'])
+
+def chi_squared_test(g, p):
+    n = len(g)
+    lam = (n-1)*p
+    degrees = np.array([degree_of(g[node]) for node in g])
+    
+    x = np.unique(degrees)
+    observed, _ = np.histogram(degrees, bins=x, density=True)
+    x = x[:-1]
+    expected = stats.poisson.pmf(x, lam)
+    
+    plt.figure(figsize=(12,8))
+    plt.title('Observed and expected frequencies')
+    plt.hist(degrees, bins=np.unique(degrees), alpha=.5, label='Observed', density=True)
+    #plt.hist(expected, bins=x, alpha=.5, label='Expected')
+    plt.plot(x,expected, label='Expected (Poisson)')
+    plt.legend()
+    plt.show()
+    
+    #expected = stats.binom.pmf(x, n, p)
+    
+    observed /= np.sum(observed)
+    expected /= np.sum(expected)
+    
+    chi_2, p_value = stats.chisquare(f_obs=observed, f_exp=expected)
+    print(f'p_value = {p_value}')
+
 def generate_graph_ER(n, p, choices: list):
-    g = {node: {'state': random.choice(choices), 'neighbors': []} for node in range(n)}
+    g = {node: {'state': np.random.choice(choices, p=[.9, .1]), 'neighbors': []} for node in range(n)}
+    
+    bar_format='{l_bar}{bar:30}{n:.0f}/{total} nodes [{elapsed}<{remaining}, {rate_fmt}]'
     
     if p < P_THRESHOLD:
-        m = int((p*n*(n-1)) // 2) # expected number of edge
+        m = int((p*n*(n-1)) // 2) # expected number of edges
         
-        for _ in tqdm(range(m), desc='Generating with ER with p little'): # O(m) -> O(n)
+        for _ in tqdm(range(m), desc='Generating with ER with p little', # O(m) -> O(n)
+                    bar_format=bar_format): 
             while True:
-                i,j = random.sample(g.keys(), 2) # pick two random nodes
-                if j in g[i]['neighbors']:
-                    continue # don't add the edge if already exists
+                i,j = tuple(random.randint(0, len(g)-1) for _ in range(2)) # pick two random nodes
+                if i==j or j in g[i]['neighbors']:
+                    continue # don't add the edge if already exists or if it's the same node
                 else:
+                    # add an edge 
                     g[i]['neighbors'].append(j)
                     g[j]['neighbors'].append(i)
                     break
     else:    
-        for i in tqdm(range(n), desc='Generating with ER'): # O(n)
+        for i in tqdm(range(n), desc='Generating with ER', bar_format=bar_format): # O(n)
             for j in range(i + 1, n): # O(log(n))
                 if random.random() < p:
                     g[i]['neighbors'].append(j)
@@ -56,7 +94,7 @@ def generate_graph_ER(n, p, choices: list):
 def simulate(g):
     FES = PriorityQueue()
     
-    time_batch = SIM_TIME / 1_000
+    time_batch = 100_000
     next_batch = time_batch
     
     random_index = random.randint(0, len(g)-1)
@@ -84,15 +122,17 @@ def simulate(g):
         time = new_time
         neighbors = g[current_i]['neighbors']
         if neighbors:
-            random_neighbor = random.choice(g[current_i]['neighbors'])
+            random_neighbor = random.choice(neighbors)
         
             g[current_i]['state'] = g[random_neighbor]['state']
             
-        if time > next_batch:
+        if time > next_batch: # for resource convinience store the results only after a while
             next_batch += time_batch
-            data.append((time,len([1 for node in g.keys() if g[node]['state'] == 1]),len([-1 for node in g.keys() if g[node]['state'] == -1])))
+            data.append((time, sum(g[node]['state'] == 1 for node in g), sum(g[node]['state'] == -1 for node in g)))
         
-        FES.put((time + random.expovariate(1), random.randint(0, len(g)-1)))
+        inter_time = random.expovariate(1)
+        random_next_node = random.randint(0, len(g)-1)  
+        FES.put((time + inter_time, random_next_node))
         
     times = [time for time,_,_ in data]
     plus = [plus for _,plus,_ in data]
@@ -107,12 +147,24 @@ def simulate(g):
     plt.grid(True)
     plt.show()
 
-# -------------------------------------------------------------------------------------------------------#
-# VOTER MODEL
-#
-#
-choices = [-1,1]
-#g = generate_graph_ER(100_000, 10**(-4), choices)
-g = generate_graph_ER(1_000, .5, choices)
 
-simulate(g)
+if __name__ == '__main__':
+    # -------------------------------------------------------------------------------------------------------#
+    # VOTER MODEL
+    #
+    #
+    n = 100_000
+    p = 10**(-4)
+    choices = [-1,1]
+    g = generate_graph_ER(n, p, choices)
+    chi_squared_test(g, p)
+    
+    #simulate(g)
+    
+    n = 10_000
+    p = .001
+    #g = generate_graph_ER(n, p, choices)
+    #chi_squared_test(g, p)
+    
+    #g = generate_graph_ER(1_000, .5, choices)    
+    
