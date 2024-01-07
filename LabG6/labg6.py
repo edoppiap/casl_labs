ccf60d33020bfc1fe9fba02d6ad39ff7860188cf
Emanuele Pietropaolo <edoppiap@gmail.com>
2024-01-07 15:08:02 +0100

    Data for plotting graphs

--- LabG6/labg6.py
+++ LabG6/labg6.py
@@ -21,6 +21,7 @@
     Upload only the py code and the report (max 2 pages).
 """
 
+import pandas as pd
 from tqdm import tqdm
 import random
 from queue import PriorityQueue
@@ -36,17 +37,17 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser(description='Input parameters for the simulation')
 
 # Population parameters
-parser.add_argument('--prob_improve', '--p_i', type=float, default=[.01, .001], nargs='+',
+parser.add_argument('--prob_improve', '--p_i', type=float, default=[.5,.1], nargs='+',
                     help='Probability of improvement of the lifetime')
-parser.add_argument('--init_population', '--p', type=int, default=[5,10,25], nargs='+',
+parser.add_argument('--init_population', '--p', type=int, default=[5,25], nargs='+',
                     help='Number of individuals for the 1st generation')
-parser.add_argument('--improve_factor', '--alpha', type=float, default=[.1, .001], nargs='+',
+parser.add_argument('--improve_factor', '--alpha', type=float, default=[.1], nargs='+',
                     help='Improve factor that an individual can develop at birth')
-parser.add_argument('--init_lifetime', type=int, default=[1,2], nargs='+',
+parser.add_argument('--init_lifetime', type=int, default=[2], nargs='+',
                     help='Lifetime of the 1st generation')
 parser.add_argument('--repr_rate', '--lambda', type=float, default=[.1, .01, .001],
                     help='Rate at which an individual reproduces')
-parser.add_argument('--max_population', type=int, default=100,
+parser.add_argument('--max_population', type=int, default=100_000,
                     help='This semplified version need a limit otherwise will infinite grow')
 
 # Simulation parameters
@@ -69,8 +70,11 @@ class Measure:
         self.time_last_event = 0
         self.birth_per_gen = {}
         
-    def increment_gen(self, gen):
-        self.birth_per_gen[gen] = self.birth_per_gen.setdefault(gen,0)+1
+    def increment_gen_birth(self, gen):
+        self.birth_per_gen[gen]['n birth'] = self.birth_per_gen.setdefault(gen,{'n birth':0, 'tot lf':0})['n birth']+1
+        
+    def increment_gen_lf(self, gen, lf):
+        self.birth_per_gen[gen]['tot lf'] = self.birth_per_gen.setdefault(gen,{'n birth':0, 'tot lf':0})['tot lf']+lf
 
 class Individual():
     def __init__(self, birth_time, alpha, p_i, parent_lf, gen):
@@ -101,10 +105,13 @@ def gen_init_population(init_p, alpha, init_lifetime):
                        gen=0) for _ in range(init_p)]
 
 # we can schedule a new birth based on len(population) after the new_death
-def death(current_time, FES: PriorityQueue, lam, population, individual, data: Measure): 
+def death(current_time, FES: PriorityQueue, lam, population, individual, data: Measure):
+    data.average_pop += len(population)*(current_time - data.time_last_event)
+    data.time_last_event = current_time
+    data.num_death+=1
     population.remove(individual)
     
-    if len(population) > 0 and len(population) < args.max_population:
+    if len(population) > 0:
         # schedule a new birth with the new len(population)
         sum_lam = lam*len(population)
         birth_time = current_time + random.expovariate(sum_lam)
@@ -113,9 +120,13 @@ def death(current_time, FES: PriorityQueue, lam, population, individual, data: M
 # we have to schedule a new death associated to the new_born
 # we can schedule a new birth based on len(population) after the new_born
 def birth(current_time, FES: PriorityQueue, lam,  alpha, p_i, population, data: Measure):
+    data.average_pop += len(population)*(current_time - data.time_last_event)
+    data.time_last_event = current_time
     
     # a new individual can born only if there are some in the population
-    if len(population) > 0 and len(population) < args.max_population:
+    if len(population) > 0:
+        data.num_birth += 1
+        
         rand_individual = population[random.randint(0, len(population)-1)]
         new_born = Individual(birth_time=current_time,
                             alpha=alpha,
@@ -123,6 +134,8 @@ def birth(current_time, FES: PriorityQueue, lam,  alpha, p_i, population, data: 
                             parent_lf=rand_individual.lifetime,
                             gen=rand_individual.gen+1 )
         population.append(new_born)
+        data.increment_gen_birth(rand_individual.gen)
+        data.increment_gen_lf(new_born.gen, new_born.lifetime)
     
         # schedule the death associated with the new_born
         FES.put(Event(current_time + new_born.lifetime, 'death', new_born))
@@ -132,7 +145,7 @@ def birth(current_time, FES: PriorityQueue, lam,  alpha, p_i, population, data: 
     birth_time = current_time + random.expovariate(sum_lam)
     FES.put(Event(birth_time, 'birth'))
     
-def simulate(init_p, init_lifetime, alpha, lam, p_i):
+def simulate(init_p, init_lifetime, alpha, lam, p_i, data: Measure):
     FES = PriorityQueue()
     time = 0
     
@@ -141,6 +154,7 @@ def simulate(init_p, init_lifetime, alpha, lam, p_i):
                                           init_lifetime=init_lifetime)
     
     for individual in population:
+        data.increment_gen_lf(individual.gen, individual.lifetime)
         FES.put(Event(individual.lifetime, 'death', individual))
     
     # the birth process follows a Poisson distr with lam = sum(lambdas)
@@ -159,9 +173,8 @@ def simulate(init_p, init_lifetime, alpha, lam, p_i):
     #----------------------------------------------------------------#
     # EVENT LOOP
     #
-    while time < args.sim_time:
-        if FES.empty() or len(population) == 0:
-            print(f'Population extinct after {time:.2f} units of time')
+    while not FES.empty():
+        if len(population) == 0 or len(population) > args.max_population:
             break
         
         event = FES.get()
@@ -179,16 +192,63 @@ def simulate(init_p, init_lifetime, alpha, lam, p_i):
                   lam=lam,
                   alpha=alpha,
                   p_i=p_i,
-                  population=population)
+                  population=population,
+                  data=data)
         elif event.type == 'death':
             individual = event.individual
             death(current_time=time,
                   FES=FES,
                   lam=lam,
                   population=population,
-                  individual=individual)
+                  individual=individual,
+                  data=data)
         print(f'N individuals: {len(population)} - time: {time:.4f}', end='\r')
+        
+    return time
     # pbar.close()
+    
+def plot_gen_birth(gen_stats: dict, folder_path, file_str, init_p):
+    
+    plt.figure(figsize=(12,8))
+    plt.bar(list(gen_stats.keys()), [gen['n birth'] for gen in gen_stats.values()])
+    plt.ylabel(f'Total number of children')
+    plt.xlabel(f'Generation')
+    plt.title('Total number of children for generation')
+    plt.grid(True, axis='y')
+    file_name = os.path.join(folder_path, 'tot_'+file_str)
+    plt.savefig(file_name, dpi=300, bbox_inches='tight')
+    plt.close()
+    
+    av_lf = []    
+    av_num_child = []
+    for i in range(len(gen_stats)):
+        if i==0:
+            av_lf.append(gen_stats[i]['tot lf'] / init_p)
+        elif i+1 in gen_stats:
+            av_num_child.append(gen_stats[i+1]['n birth'] / gen_stats[i]['n birth'])
+            av_lf.append(gen_stats[i]['tot lf'] / gen_stats[i]['n birth'])
+        else:
+            av_num_child.append(0)
+            
+    plt.figure(figsize=(12,8))
+    plt.bar(range(len(av_lf)), av_lf)
+    plt.ylabel(f'Average lifetime')
+    plt.xlabel(f'Generation')
+    plt.title('Average lifetime for generation')
+    plt.grid(True, axis='y')
+    file_name = os.path.join(folder_path, 'lf_'+file_str)
+    plt.savefig(file_name, dpi=300, bbox_inches='tight')
+    plt.close()
+    
+    plt.figure(figsize=(12,8))
+    plt.bar(range(len(av_num_child)), av_num_child)
+    plt.ylabel(f'Average number of children')
+    plt.xlabel(f'Generation')
+    plt.title('Average number of children for generation')
+    plt.grid(True, axis='y')
+    file_name = os.path.join(folder_path, 'av_'+file_str)
+    plt.savefig(file_name, dpi=300, bbox_inches='tight')
+    plt.close()
 
 #-----------------------------------------------------------------------------------------------------------#
 # MAIN METHOD
@@ -210,6 +270,8 @@ if __name__ == '__main__':
     
     random.seed(args.seed)
     
+    results = []
+    
     params = [(init_p, init_lifetime, alpha, lam, p_i) for init_p in args.init_population 
               for init_lifetime in args.init_lifetime 
               for alpha in args.improve_factor
@@ -219,9 +281,34 @@ if __name__ == '__main__':
     print(f'params combination: {len(params)}')
     
     for init_p, init_lifetime, alpha, lam, p_i in params:
+        data = Measure()
         print(f'Simulate with init_p={init_p} - init_lifetime={init_lifetime} - alpha={alpha} - lambda={lam} - p_i={p_i}')
-        simulate(init_p=init_p,
+        end_time = simulate(init_p=init_p,
                 init_lifetime=init_lifetime,
                 alpha=alpha,
                 lam=lam,
-                p_i=p_i)
+                p_i=p_i,
+                data=data)
+        
+        result = {
+            'init_p': init_p,
+            'init_lifetime':init_lifetime,
+            'alpha': alpha,
+            'lambda':lam,
+            'p_i':p_i,
+            'num_birth': data.num_birth,
+            'num_death': data.num_death,
+            'average_pop': data.average_pop/end_time,
+            'gen_num': len(data.birth_per_gen),
+            'end_time': end_time
+        }
+        results.append(pd.DataFrame([result]))
+        
+        if len(data.birth_per_gen) > 7:
+            file_str = f'in_p_{init_p}_in_lt_{init_lifetime}_a_{alpha}_l_{lam}_p_i_{p_i}.'
+            plot_gen_birth(data.birth_per_gen, folder_path, file_str, init_p)
+    
+    result_df = pd.concat(results, ignore_index=True)
+    
+    file_name = os.path.join(folder_path, 'results.csv')
+    result_df.to_csv(file_name)
