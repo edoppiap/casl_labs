d6fda3aa34700f4a16892b7d8337986d4b905056
Emanuele Pietropaolo <edoppiap@gmail.com>
2024-01-18 14:36:50 +0100

    Stochastic simulation working

--- LabL7/labl7.py
+++ LabL7/labl7.py
@@ -25,6 +25,8 @@ import os, datetime
 import random
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import copy
+import numpy as np
 
 #-----------------------------------------------------------------------------------------------------------#
 # INPUT PARAMETERS
@@ -37,9 +39,9 @@ def get_input_parameters():
     # simulation parameters
     parser.add_argument('--initial_population', type=int, default=50_000_000,
                         help='Initial number of individual in the system')
-    parser.add_argument('--repr_rate', type=float, default=.3,
+    parser.add_argument('--repr_rate', type=float, default=4,
                         help='Reproducibility rate (R(0)) for the simulation') 
-    parser.add_argument('--recov_rate', type=float, default=.1,
+    parser.add_argument('--recov_rate', type=float, default=1/14,
                         help='Recovery rate')
     parser.add_argument('--fatality_rate', type=float, default=.03,
                         help='Fatality rate')
@@ -51,7 +53,7 @@ def get_input_parameters():
                         help='Maximum number of death in one year')
     parser.add_argument('--sim_time', type=int, default=365,
                         help='Period of time to simulate')
-    parser.add_argument('--restr_rate', type=float, default=[.5,.4,.35,.3], nargs='+',
+    parser.add_argument('--restr_rate', type=float, default=[1], nargs='+',
                         help='Float between 0 and 1 representing how strong the restriction are')
 
     # utility parameters
@@ -91,7 +93,7 @@ def initial_population(n):
     population = {
         'time': [0],
         'susceptible':[n], # at t=0 the only variable is the susceptible one
-        'infected':[int(n*.001)], # starts the simulation with a little number of infected
+        'infected':[int(1)], # starts the simulation with a little number of infected
         'hospitalized':[0],
         'recovered':[0],
         'deaths':[0]
@@ -99,11 +101,85 @@ def initial_population(n):
     
     return population
 
+def marked_poisson(intensity, gamma):
+    points = np.random.poisson(intensity)
+    
+    marks = np.random.rand(points)
+    p_i = intensity / gamma
+    
+    T_i = np.random.rand(points)
+    selected_T_i = T_i[marks < p_i]
+    
+    return selected_T_i
+
 #-----------------------------------------------------------------------------------------------------------#
-# SIMULATION
+# STOCHASTIC SIMULATION 
 #
 #
-def simulate(population: dict, max_time, lam, gamma, restr_rate, death_rate, hosp_places):
+def simulate(population: dict, max_time, r_0, gamma, restr_rate, death_rate, hosp_places):
+    #---------------------------------#
+    # CALCULATE LAMBDA BASED ON R(t=0)
+    #   
+    lam = (r_0 * gamma) / population['susceptible'][0]
+    
+    loop = tqdm(range(1,max_time), desc='Simulating with Point process')
+    
+    #------------------------------------#
+    # SIM LOOP
+    #
+    #
+    for day in loop:
+        S_t = population['susceptible'][-1]
+        I_t = population['infected'][-1]
+        H_t = population['hospitalized'][-1]
+        R_t = population['recovered'][-1]
+        D_t = population['deaths'][-1]
+        
+        infected_intensity = lam * S_t * I_t / gamma # new infected depends only from I_t
+        removed_intensity = gamma * (I_t + H_t) # new removed depends from both I_t and H_t
+        
+        #---------------------------------#
+        # CALCULATE NEW DAYS INFECTED/REMOVED INDIVIDUALS
+        #
+        new_infected = marked_poisson(infected_intensity, gamma)
+        try:
+            new_removed = marked_poisson(removed_intensity, gamma)
+        except ValueError:
+            print(f'{removed_intensity = } \n{I_t = } \n {H_t = }')
+            return 
+        
+        new_S = max(0, S_t - len(new_infected))
+        new_I = max(0, I_t + len(new_infected) - len(new_removed) if new_S > 0 else I_t + S_t - len(new_removed))
+        
+        #---------------------------------#
+        # SEPARETE INFECTED INTO COMMON AND HOSPITALIZED
+        #
+        new_I_effective = int(new_I * (1 - args.hospitalized_percentage))
+        new_H = int(new_I * args.hospitalized_percentage)
+        
+        #---------------------------------#
+        # SEPARETE REMOVED INTO RECOVERED AND DEATHS
+        #
+        new_R_effective = int(R_t + len(new_removed) * (1 - death_rate))
+        new_D = int(D_t + len(new_removed) * death_rate)
+        
+        #---------------------------------#
+        # STORE RESULTS
+        #
+        population['time'].append(day)
+        population['susceptible'].append(new_S)
+        population['infected'].append(new_I_effective)
+        population['hospitalized'].append(new_H)
+        population['deaths'].append(new_D)
+        population['recovered'].append(new_R_effective)
+        
+        loop.set_postfix_str(f'Susceptible = {S_t}')
+
+#-----------------------------------------------------------------------------------------------------------#
+# MEAN FIELD SIMULATION
+#
+#
+def simulate_mean_field(population: dict, max_time, r_0, gamma, restr_rate, death_rate, hosp_places):
     S = population['susceptible'][0]
     I = population['infected'][0]
     R = population['recovered'][0]
@@ -111,17 +187,19 @@ def simulate(population: dict, max_time, lam, gamma, restr_rate, death_rate, hos
     D = population['deaths'][0]
     N = S+I+R
     
+    lam = (r_0 * gamma) / population['susceptible'][0]
+    
     #------------------------------------#
     # SIM LOOP
     #
     #
-    for day in range(1,max_time):
+    for day in tqdm(range(1,max_time), desc='Simulating with mean_field'):
         
         #---------------------------------#
         # CALCULATE NEW DAYS INFECTED INDIVIDUALS
         #
         new_removed = gamma*(population['infected'][day-1] + population['hospitalized'][day-1])
-        new_infected = (lam*restr_rate*population['susceptible'][day-1]*population['infected'][day-1])/N
+        new_infected = (lam*restr_rate*population['susceptible'][day-1]*population['infected'][day-1])
         S -= new_infected
         
         #---------------------------------#
@@ -155,16 +233,23 @@ def simulate(population: dict, max_time, lam, gamma, restr_rate, death_rate, hos
 # PLOT GRAPHS
 #
 #
-def plot_results(population: dict = None, populations: list = None):
+def plot_results(population:dict=None, mean_pop: dict = None, populations: list = None):
     
-    if population:
+    if population or mean_pop:
         plt.figure(figsize=(12,8))
         #plt.plot(population['time'], population['susceptible'], label='Susceptible')
-        plt.plot(population['time'], population['infected'], label='Infected', c='orange')
-        #plt.plot(population['time'], population['recovered'], label='Recovered', c='g')
-        plt.plot(population['time'], population['deaths'], label='Deaths', c='black')
-        plt.plot(population['time'], population['hospitalized'], label='Hospitalized')
-        plt.axhline(y=args.death_limit, color='r', linestyle='--', label='Desired number of deaths')
+        if mean_pop:
+            plt.plot(mean_pop['time'], mean_pop['infected'], label='Infected', c='orange', linestyle='--')
+            plt.plot(mean_pop['time'], mean_pop['recovered'], label='Recovered', c='g', linestyle='--')
+            plt.plot(mean_pop['time'], mean_pop['deaths'], label='Deaths', c='black', linestyle='--')
+            plt.plot(mean_pop['time'], mean_pop['hospitalized'], label='Hospitalized', linestyle='--')
+        if population:
+            plt.plot(population['time'], population['infected'], label='Infected', c='orange')
+            #plt.plot(population['time'], population['susceptible'], label='Susceptible', c='b')
+            plt.plot(population['time'], population['recovered'], label='Recovered', c='g')
+            plt.plot(population['time'], population['deaths'], label='Deaths', c='black')
+            plt.plot(population['time'], population['hospitalized'], label='Hospitalized')
+        plt.axhline(y=args.death_limit, color='r', linestyle='dashdot', label='Desired number of deaths')
         plt.xlabel('Time')
         plt.ylabel('N individuals')
         plt.legend()
@@ -173,8 +258,8 @@ def plot_results(population: dict = None, populations: list = None):
         
     if populations:
         plt.figure(figsize=(12,8))
-        for p,population in populations:
-            plt.plot(population['time'], population['deaths'], label=f'Deaths with p = {p}')
+        for p,mean_pop in populations:
+            plt.plot(mean_pop['time'], mean_pop['deaths'], label=f'Deaths with p = {p}')
         plt.axhline(y=args.death_limit, color='r', linestyle='--', label='Desired max number of deaths')
         plt.xlabel('Time')
         plt.ylabel('N individuals')
@@ -183,8 +268,8 @@ def plot_results(population: dict = None, populations: list = None):
         plt.show()
         
         plt.figure(figsize=(12,8))
-        for p,population in populations:
-            plt.plot(population['time'], population['hospitalized'], label=f'Hospitalized individuals with p = {p}')
+        for p,mean_pop in populations:
+            plt.plot(mean_pop['time'], mean_pop['hospitalized'], label=f'Hospitalized individuals with p = {p}')
         plt.axhline(y=args.hosp_places, color='r', linestyle='--', label='Max places in hospitals')
         plt.xlabel('Time')
         plt.ylabel('N individuals')
@@ -212,15 +297,28 @@ if __name__ == '__main__':
     for restr_rate in args.restr_rate:
         population = initial_population(args.initial_population)
         
-        simulate(population, 
+        mean_pop = copy.deepcopy(population)
+        
+        simulate(population=population,
                  max_time=args.sim_time, 
-                 lam=args.repr_rate, 
+                 r_0=args.repr_rate, 
                  gamma=args.recov_rate, 
                  death_rate=args.fatality_rate, 
                  restr_rate=restr_rate,
                  hosp_places=args.hosp_places)
         
-        #plot_results(population=population)
+        simulate_mean_field(population=mean_pop, 
+                 max_time=args.sim_time, 
+                 r_0=args.repr_rate, 
+                 gamma=args.recov_rate, 
+                 death_rate=args.fatality_rate, 
+                 restr_rate=restr_rate,
+                 hosp_places=args.hosp_places)
+        # plot_results(mean_pop=mean_pop)
+        
+        # plot_results(population=population)
+        
+        plot_results(population=population,mean_pop=mean_pop)
         
         populations.append((restr_rate,population))
-    plot_results(populations=populations)
+    #plot_results(populations=populations)
